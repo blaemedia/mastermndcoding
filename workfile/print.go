1ef047ef6242588c67b9782e00103a66ea164113
Emmanuel <blaemedia26@gmail.com>
2024-02-05 02:57:06 -1200

    Mastermnd software engineering course

---
+++ workfile/print.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	name := "Welcome to GO language"
+
+	fmt.Println(name)
+
+	name = "Emmanuel"
+
+	fmt.Println(name)
+
+	var nameListing []string
+	nameListing = []string{"Emmanuel", "Shine", "Babatunde"}
+
+	fmt.Print(nameListing[2])
+}
